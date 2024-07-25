6b390537eb1af55430ff6d949035d8cfee30a9b9
Chris <github@prettycode.org>
2024-07-25 11:37:19 -0700

    fix: Retool eslint's config so avoid ignore pattern in npm script

--- node/eslint.config.mjs
+++ node/eslint.config.mjs
@@ -1,40 +1,16 @@
-import typescriptEslint from '@typescript-eslint/eslint-plugin';
-import globals from 'globals';
-import tsParser from '@typescript-eslint/parser';
-import path from 'node:path';
-import { fileURLToPath } from 'node:url';
-import js from '@eslint/js';
-import { FlatCompat } from '@eslint/eslintrc';
+import eslint from '@eslint/js';
+import tseslint from 'typescript-eslint';
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-const compat = new FlatCompat({
-    baseDirectory: __dirname,
-    recommendedConfig: js.configs.recommended,
-    allConfig: js.configs.all
-});
-
-export default [
-    ...compat.extends('eslint:recommended', 'plugin:@typescript-eslint/recommended'),
+export default tseslint.config(
+    eslint.configs.recommended,
+    ...tseslint.configs.recommended,
     {
-        plugins: {
-            '@typescript-eslint': typescriptEslint
-        },
-
-        languageOptions: {
-            globals: {
-                ...Object.fromEntries(Object.entries(globals.browser).map(([key]) => [key, 'off']))
-            },
-
-            parser: tsParser,
-            ecmaVersion: 'latest',
-            sourceType: 'module'
-        },
-
+        ignores: ['dist']
+    },
+    {
         rules: {
             '@typescript-eslint/explicit-function-return-type': 'error',
             '@typescript-eslint/no-explicit-any': 'error',
-
             '@typescript-eslint/no-unused-vars': [
                 'error',
                 {
@@ -44,4 +20,4 @@ export default [
             ]
         }
     }
-];
+);
